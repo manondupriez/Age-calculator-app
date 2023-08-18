0f3e077c139af86ad1e147a97747851f3c494a3f
Manon Dupriez <manondupriez@outlook.fr>
2023-08-18 23:37:08 +0200

    Add JavaScript comments

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,14 @@
-// Retire ou ajoute une ligne de séparation selon la largeur de l'écran
+// VISUAL PART
 
+// Hides or shows the dividing line based on screen width
 $line = document.getElementById('line');
 
 function resizeSubmitArea() {
-
     if (window.innerWidth > 900) {
+        // Hides the dividing line for larger screens
         line.classList.add('d-none');
     } else {
+        // Shows the dividing line for smaller screens
         line.classList.remove('d-none');
     }
 }
@@ -14,22 +16,25 @@ function resizeSubmitArea() {
 resizeSubmitArea();
 window.addEventListener('resize', resizeSubmitArea);
 
-// Réajuste la taille et l'épaisseur de l'icone du bouton de soumission selon la largeur de l'écran
-
+// Adjusts the size and thickness of the submit button icon according to screen width
 $submitButtonIcon = document.getElementById('arrowDownIcon');
 $submitButtonIconPaths = document.querySelectorAll('#arrowDownIcon path');
 
 function resizeSubmitButtonIcon() {
     if (window.innerWidth > 900) {
+        // Enlarges icon for larger screens
         $submitButtonIcon.setAttribute('width', '44');
         $submitButtonIcon.setAttribute('height', '44');
         $submitButtonIconPaths.forEach(path => {
+            // Decreases stroke width for larger screens
             path.setAttribute('stroke-width', '1');
         });
     } else {
+        // Shrinks icon for smaller screens
         $submitButtonIcon.setAttribute('width', '24');
         $submitButtonIcon.setAttribute('height', '24');
         $submitButtonIconPaths.forEach(path => {
+            // Increases stroke width for smaller screens
             path.setAttribute('stroke-width', '2');
         });
     }
@@ -37,3 +42,6 @@ function resizeSubmitButtonIcon() {
 
 resizeSubmitButtonIcon();
 window.addEventListener('resize', resizeSubmitButtonIcon);
+
+// LOGIC PART
+
