4fb1a9e1f1255e9219d5f901d2ffdd1c40aea97a
Manon Dupriez <manondupriez@outlook.fr>
2023-08-20 22:12:06 +0200

    Comment and reorganize JS code. TODO: make calculation part

--- scripts/main.js
+++ scripts/main.js
@@ -1,57 +1,61 @@
-// VISUAL PART
+// ********** VISUAL PART (unrelated to form) **********
 
 // Hides or shows the dividing line based on screen width
 const line = document.getElementById('line');
-
 function resizeSubmitArea() {
     if (window.innerWidth > 900) {
-        // Hides the dividing line for larger screens
+        // Hides the dividing line on larger screens
         line.classList.add('d-none');
     } else {
-        // Shows the dividing line for smaller screens
+        // Shows the dividing line on smaller screens
         line.classList.remove('d-none');
     }
 }
-
 resizeSubmitArea();
 window.addEventListener('resize', resizeSubmitArea);
 
 // Adjusts the size and thickness of the submit button icon according to screen width
 const submitButtonIcon = document.getElementById('arrowDownIcon');
 const submitButtonIconPaths = document.querySelectorAll('#arrowDownIcon path');
-
 function resizeSubmitButtonIcon() {
     if (window.innerWidth > 900) {
+
         // Enlarges icon for larger screens
         submitButtonIcon.setAttribute('width', '44');
         submitButtonIcon.setAttribute('height', '44');
         submitButtonIconPaths.forEach(path => {
+
             // Decreases stroke width for larger screens
             path.setAttribute('stroke-width', '1');
+
         });
     } else {
+
         // Shrinks icon for smaller screens
         submitButtonIcon.setAttribute('width', '24');
         submitButtonIcon.setAttribute('height', '24');
         submitButtonIconPaths.forEach(path => {
+
            // Increases stroke width for smaller screens
             path.setAttribute('stroke-width', '2');
+
         });
     }
 }
-
 resizeSubmitButtonIcon();
 window.addEventListener('resize', resizeSubmitButtonIcon);
 
-// LOGIC PART
+// ********** LOGIC PART **********
+
+    // ********** Form entries validation **********
 
 document.addEventListener('DOMContentLoaded', () => {
 
+    // Gets the current date and year
     const today = new Date();
     const currentYear = today.getFullYear();
 
     const form = document.getElementById('form');
-    let canSubmit = false;
     const formElements = [
         {
             id: 'dayInput',
@@ -86,38 +90,43 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     ];
 
+    let canSubmit = false;
     formElements.forEach(element => {
 
         document.getElementById(element.id).addEventListener('input', () => {
 
+            // If an element is valid according to its own conditions
             if (element.isValid(document.getElementById(element.id).value.trim())) {
                 
+                // Sets the valid element property at true
                 element.valid = true;
-                let AllFieldsAreValid = true;
 
+                let AllFieldsAreValid = true;
                 formElements.forEach(element => {
+                    // Updates allFieldsAreValid depending on each valid or invalid element
                     AllFieldsAreValid &= element.valid;
                 });
 
+                // Calls the entire date validation function if all the fields are valid
                 if (AllFieldsAreValid) {
                     validateBirthdayDate();
                 }
 
                 // Updates display
                 removeErrorDisplay(element);
-
                 // Removes error messages
                 removeErrorMessage(element);
 
             } else {
 
+                // Sets canSubmit at false which prevents form submission
                 canSubmit = false;
+                // Resets the valid element property at false
                 element.valid = false;
 
-                // Calls a function which updates display if invalid
+                // Updates display
                 addErrorDisplay(element);
-
-                // Calls a function which updates error messages
+                // Updates error messages
                 updateErrorMessage(element);
 
             }
@@ -130,6 +139,7 @@ document.addEventListener('DOMContentLoaded', () => {
         return (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
     }
 
+    // Checks that the entire date is valid
     const validateBirthdayDate = () => {
 
         const dayInput = document.getElementById('dayInput').value.trim();
@@ -137,6 +147,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const yearInput = document.getElementById('yearInput').value.trim();
 
         const maxDaysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+        // Sets 29 days in february in case of leap year
         if (isLeapYear(yearInput)) {
             maxDaysInMonth[1] = 29;
         }
@@ -207,7 +218,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
             document.getElementById(element.id).nextElementSibling.textContent = element.errorMessages.invalidEntry2;
 
-            // If all the fields are OK but the whole date is wrong
+            // If all the fields are valid but the whole date is wrong (ex: leap year)
         } else if (false === validateBirthdayDate()) {
 
             document.getElementById('dayInput').nextElementSibling.textContent = 'Must be a valid date';
@@ -218,10 +229,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const removeErrorMessage = (element) => {
 
+        // Removes error messages
         document.getElementById(element.id).nextElementSibling.classList.add('d-none');
         document.getElementById(element.id).nextElementSibling.textContent = '';
 
     }
+
+        // ********** AGE CALCULATION **********
+
+    
+
+        // ********** FORM SUBMISSION **********
 
     form.addEventListener('submit', event => {
 
